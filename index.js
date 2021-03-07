675d17893615633fffd8a92e9cc7827f361f9600
vakhtangaA <vakhtang.chitauri@gmail.com>
2021-03-07 16:17:16 +0400

    added static to express

--- index.js
+++ index.js
@@ -97,6 +97,8 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
+app.use(express.static("build"));
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
